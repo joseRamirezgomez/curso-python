8b0fd10c1049f1a3fe000626bebf753483e2787d
JoseRamirez <joseramirezgomez00@gmail.com>
2024-03-21 11:28:22 -0500

    operadores logicos

--- operadores/op_logicos.py
+++ operadores/op_logicos.py
@@ -1,8 +1,19 @@
 #AND
-
-
+Resultado = True & True #true
+Resultado2 = False & True #false
+Resultado3 = True & False  #False
+Resultado4 = False & False #False
 
 #OR
 
+Resultado5 = True | True #True
+Resultado6 = False | True #True
+Resultado7 = True | False #True
+Resultado8 = False | False #False
 
 #NOT
+
+Resultado9 =  not True #False
+Resultado10 = not False #True
+
+print(Resultado10)
