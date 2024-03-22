dca9664235af9f666a5a5a49c1e4a876b4261daf
JoseRamirez <joseramirezgomez00@gmail.com>
2024-03-22 11:09:14 -0500

    inputs

---
+++ inputs/input.py
@@ -0,0 +1,6 @@
+#pedimos datos al usualio
+nombre = input("hola, como es tu nombre? : ")
+
+#f-strings : print(f"hola {nombre}")
+
+print(f'el nombre es {nombre}')
---
+++ inputs/inputs_numeros.py
@@ -0,0 +1,11 @@
+#numero
+
+numero= input("digita un numero: ")
+
+#con int() se convierte el numero a entero
+#resultado = int(numero)*2
+
+#con float() se convierte el numero a flotante
+resultado = float(numero)*2
+
+print(f'el resultado es {resultado}')
