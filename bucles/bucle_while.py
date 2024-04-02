dc62744b05d01f8f868bcc7dec825fa1cbf16077
JoseRamirez <joseramirezgomez00@gmail.com>
2024-04-02 09:37:34 -0500

    bucle while

---
+++ bucles/bucle_while.py
@@ -0,0 +1,8 @@
+contador = 0
+
+#mietras que la condicion se cumpla , el bucle sigue ejecutandose
+while contador < 15:
+    contador+=1
+    print(contador)
+    
+print("Acabamos de salir del bucle")
