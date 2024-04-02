2d82861cc9f66ab18a7a9dd4879049d49ad8cc3e
JoseRamirez <joseramirezgomez00@gmail.com>
2024-04-02 10:06:31 -0500

    build_in

---
+++ funciones/build_in.py
@@ -0,0 +1,14 @@
+numeros = [4,7,1,42,15]
+
+#encontrando el numero más alto
+numero_max = max(numeros)
+print(numero_max)
+
+#encontrando el numero más bajo
+numero_min = min(numeros)
+print(numero_min)
+
+#redondeando un numero con round
+numero = 13.2648987
+redondeado = round(numero,3)#el ,3 indica los decimales a redondear
+print(redondeado)
