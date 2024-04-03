5ded2c861bf56cbab93cd5bb89c03745a6b40bfc
JoseRamirez <joseramirezgomez00@gmail.com>
2024-04-03 10:42:52 -0500

    Ejercicio 2.3 fibonacci

---
+++ ejercicios/ejercicio2.3.py
@@ -0,0 +1,13 @@
+#funcion fibonacci enntre 0 y numero dado
+
+def fibonacci(num):
+    a,b = 0,1
+    fibonacci_lista = [0]
+    for i in range(num):
+        if b > num: return fibonacci_lista
+        else:
+            fibonacci_lista.append(b)
+            a,b = b,a+b
+
+resultado = fibonacci(34)
+print(resultado)
